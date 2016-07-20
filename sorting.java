fdf6cdca65f9971183e5b067ed98d9eb2b98a228
sabitharevathi <sabitharevathi@gmail.com>
2016-07-20 16:49:15 +0530

    Add files via upload

---
+++ sorting.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class sorting {
+	public static void main(String p[]){
+		int i;
+		Scanner abc=new Scanner(System.in);
+		i=abc.nextInt();
+		if(i>=0){
+			System.out.println("no. is postive"+i);
+		}
+		else if(i<0){System.out.println("no. is negative"+i);
+		}
+			}
+		}
+
+
