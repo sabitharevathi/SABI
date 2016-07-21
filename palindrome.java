7e6aa445e8fe9eb1c6c318925944746625bc9443
sabitharevathi <sabitharevathi@gmail.com>
2016-07-21 09:47:41 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class palindrome {
 	public static void main(String o[])
 	{
-String input1;
+int  input1;
 
 Scanner abc=new Scanner(System.in);
-input1=abc.nextLine();
+input1=abc.nextInt();
 StringBuffer br=new StringBuffer(input1);
 
 br.reverse();
