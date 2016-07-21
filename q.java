7e7cb81583fe7625b2109c8f9ff28117b50e5854
sabitharevathi <sabitharevathi@gmail.com>
2016-07-21 09:38:31 +0530

    Add files via upload

---
+++ q.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class q {
+	public static void main(String jk[]){
+		String s;
+		Scanner ss=new Scanner(System.in);
+		s=ss.nextLine();
+		for(int i=0;i<1;i++){
+		if(s=="q"){
+			break;
+		}
+			
+		}
+
+	}
+}
