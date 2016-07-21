4f438fa2718555c40d9bae32665eb986b536ff2e
sabitharevathi <sabitharevathi@gmail.com>
2016-07-21 09:41:39 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class palindrome {
+	public static void main(String o[])
+	{
+String input1;
+
+Scanner abc=new Scanner(System.in);
+input1=abc.nextLine();
+StringBuffer br=new StringBuffer(input1);
+
+br.reverse();
+String k=new String(br);
+System.out.println(br);
+	if(input1.equals(k)){
+		System.out.println("palindrome");
+    }
+	else
+	{
+		System.out.println("not palindrome");
+	}}}
