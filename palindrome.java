392f38c59a734a2f42ce58dd1b8b0e8f7383b79c
sabitharevathi <sabitharevathi@gmail.com>
2016-07-21 09:49:42 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -4,19 +4,14 @@ import java.util.Scanner;
 public class palindrome {
 	public static void main(String o[])
 	{
-int  input1;
+String input1;
 
 Scanner abc=new Scanner(System.in);
-input1=abc.nextInt();
+input1=abc.next();
 StringBuffer br=new StringBuffer(input1);
 
 br.reverse();
 String k=new String(br);
 System.out.println(br);
-	if(input1.equals(k)){
-		System.out.println("palindrome");
-    }
-	else
-	{
-		System.out.println("not palindrome");
-	}}}
+	}}
+	
