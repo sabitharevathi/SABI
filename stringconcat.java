8d1fd792526b97b799c2eaff29ad9f54bec5db60
sabitharevathi <sabitharevathi@gmail.com>
2016-07-22 14:40:28 +0530

    Update stringconcat.java

--- stringconcat.java
+++ stringconcat.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 public class stringconcat {
 	 public static void main(String [] args){
-		    String n;
+		    String na;
 		   
 		    Scanner ss=new Scanner(System.in);
-		    n=ss.nextLine();
-		    String n1;
+		    na=ss.nextLine();
+		    String n1a;
 			   
 		    Scanner as=new Scanner(System.in);
-		    n1=as.nextLine();
-	 String s2=n+n1;
+		    n1a=as.nextLine();
+	 String s2a=na+n1a;
 	 
-	 System.out.println(s2);
+	 System.out.println(s2a);
 	 }
 }
