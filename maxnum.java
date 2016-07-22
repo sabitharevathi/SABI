baed69b031cd93652c1f0b500257dcd783f533a2
sabitharevathi <sabitharevathi@gmail.com>
2016-07-22 14:43:10 +0530

    Update maxnum.java

--- maxnum.java
+++ maxnum.java
@@ -6,27 +6,27 @@
 
 	public class maxnum {
 		public static void main(String [ ] arg) {
-			int a[]=new int[10];
+			int  B[]=new int[10];
 			Scanner name=new Scanner(System.in);
 		for(int i=0;i<3;i++)
 		{
-			a[i]=name.nextInt();
+		B[i]=name.nextInt();
 		}
 		
 			for(int j=0;j<3;j++)
 		{
 				for(int k=j+1;k<3;k++)
 				{
-				if(a[j]>a[k])
+				if(B[j]>B[k])
 				{
-					int tmp=a[j];
-					a[j]=a[k];
-					a[k]=tmp;
+					int tmp=B[j];
+					B[j]=B[k];
+					B[k]=tmp;
 					
 				}
 			}
 			}
-		System.out.println(a[2]);
+		System.out.println(B[2]);
 			
 		
 			
