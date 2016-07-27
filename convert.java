04062ded1323612fe1c4cc871844f4cb6ed854cd
sabitharevathi <sabitharevathi@gmail.com>
2016-07-27 18:33:48 +0530

    Add files via upload

---
+++ convert.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+import java.util.Scanner;
+public class convert  {
+public static void main(String j[]){
+	
+		String s;
+	
+	Scanner abc=new Scanner(System.in);
+	s=abc.next();
+	int i;
+	i=Integer.parseInt(s);
+	System.out.println(i );
+
+}
+}
+
+
+
