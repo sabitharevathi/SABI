e68ec8bec716059e49f9627c4c41ccda91c054c4
sabitharevathi <sabitharevathi@gmail.com>
2016-07-27 18:11:50 +0530

    Add files via upload

---
+++ NUMERIC.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class NUMERIC {
+public static void main(String j[]){
+	try{
+		String s;
+	
+	Scanner abc=new Scanner(System.in);
+	s=abc.next();
+	int i;
+	i=Integer.parseInt(s);
+	System.out.println("true" );
+	
+}catch(Exception e){
+	System.out.println("flase");
+}
+}
+}
