67b9dd3464351d359555ef8466633a6a7f4a742b
sabitharevathi <sabitharevathi@gmail.com>
2016-07-28 09:17:02 +0530

    Add files via upload

---
+++ word.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class word {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a;
+		Scanner abc=new Scanner(System.in);
+		a=abc.nextLine(); 
+		String l[]=a.split(" ");
+		int k=l.length;
+		System.out.println(k);
+
+	}
+
+}
