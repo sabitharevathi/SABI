2ad2ed8a17d5bb6a0bedffae5be5bc9e29b659c3
sabitharevathi <sabitharevathi@gmail.com>
2016-07-28 15:14:46 +0530

    Add files via upload

---
+++ subsplit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class subsplit {
+public static void main(String dd[]){
+	String a;		Scanner aa=new Scanner(System.in);
+	a=aa.next();
+	String b[]=new String[11];
+int k=0,count=0;
+
+	
+		for(int j=1;j<=a.length()-1;j++){
+			System.out.println("p"+a.charAt(j));
+		
+				}
+			
+		
+	
+	
+}
+}
