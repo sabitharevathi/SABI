249324657f469a1ddd43855ce63ac6fba147bbb8
sabitharevathi <sabitharevathi@gmail.com>
2016-07-28 09:13:51 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class sum {
+	public static void main(String g[]){
+		String s;
+		Scanner abc=new Scanner(System.in);
+		s=abc.next();
+		int out;
+		out=Integer.parseInt(s);
+		System.out.println(out);
+		
+}
+}
