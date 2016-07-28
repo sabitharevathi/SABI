385ee2441265c4df34386d96d5a6d10c8bad351d
sabitharevathi <sabitharevathi@gmail.com>
2016-07-28 09:12:52 +0530

    Add files via upload

---
+++ line.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class line {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String k;
+Scanner abc=new Scanner(System.in);
+k=abc.nextLine(); 
+String l[]=k.split(".");
+//System.out.println(l[2]);
+int a=l.length;
+System.out.println(a);
+	}
+
+}
