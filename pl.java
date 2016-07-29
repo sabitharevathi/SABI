89f1c55cde6469b3a183b511555a04ad3c82af21
sabitharevathi <sabitharevathi@gmail.com>
2016-07-29 10:34:23 +0530

    Update pl.java

--- pl.java
+++ pl.java
@@ -1,36 +1,36 @@
 import java.util.Scanner;
 
 
-public class rotated {
-	public static void main(String kd[]){
-		int a,k=0;
+public class ro {
+	public static void main(String kd1[]){
+		int a1,k1=0;
 		System.out.println("enter the size of array");
 		Scanner abc=new Scanner(System.in);
-		a=abc.nextInt();
-		int b[]=new int[a];
-		int aa[]=new int [a];
+		a1=abc.nextInt();
+		int b1[]=new int[a1];
+		int aa1[]=new int [a1];
 		System.out.println("enter the array");
-		for(int i=0;i<a;i++){
-			b[i]=abc.nextInt();
+		for(int i=0;i<a1;i++){
+			b1[i]=abc.nextInt();
 		}
 		System.out.println("enter the element to rotate");
-		int dk;
-		dk=abc.nextInt();
-		for(int i=0;i<=a-1;i++){
-			if(b[i]==dk){
-				while(i<=a-1){
+		int dk1;
+		dk1=abc.nextInt();
+		for(int i=0;i<=a1-1;i++){
+			if(b1[i]==dk1){
+				while(i<=a1-1){
 					i++;
 					//System.out.println(i);
 					aa[k]=b[i];
 					System.out.println(aa[k]);
 					k++;
 					
-					if(i==a-1){
+					if(i==a1-1){
 						i=0;
-						while(i<=dk-1){
-							aa[k]=b[i];
-							System.out.println(aa[k]);
-							k++;
+						while(i<=dk1-1){
+							aa1[k]=b1[i];
+							System.out.println(aa1[k]);
+							k1++;
 							i++;
 						}
 						
