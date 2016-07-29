acf2bb736858da9e78d3631d5507055a4b316f54
sabitharevathi <sabitharevathi@gmail.com>
2016-07-29 17:14:56 +0530

    Add files via upload

--- print.java
+++ print.java
@@ -13,26 +13,27 @@ public class print {
 		for(int i=0;i<l-1;i++){
 			for(int j=i+1;j<i+2;j++){
 				if(j<=s.length()-1){
-					
+					char arr[];
 				if(s.charAt(i)==(s.charAt(j))){
 					ss[k]=s.charAt(i);
 				k++;
 					ss[k]='*';
-					k++;
-					
+										
 				}
 				else
-				{
-					
+				{		
 					
 				ss[k]=s.charAt(i);
 				
-				k++;
-					
-				}
+									
+				}k++;
+
 			
 		}}}
-	for(int i=0;i<=k-1;i++){
+		System.out.println(s);
+		ss[k]=s.charAt(s.length()-1);
+		System.out.println(ss[k]);
+	for(int i=0;i<=k;i++){
 		System.out.print(ss[i]);
 	}
 	}
