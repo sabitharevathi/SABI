f4866b9fd6e2ad3bdeb3b899a764b0661a6bebdf
sabitharevathi <sabitharevathi@gmail.com>
2016-09-01 12:11:25 +0530

    Add files via upload

---
+++ player52.java
@@ -0,0 +1,12 @@
+package player;
+
+public class player52 {
+	public static void main(String arg[]){
+		String st1=new String("welcome to guvi");
+		String st2=new String("welcome to the world");
+		System.out.println("length of st1 ="+st1.length());
+
+		System.out.println("length of st2 ="+st2.length());
+	
+	}
+}
