0a59ad8592d870829f088b3a1bf05de2c99e30d9
sabitharevathi <sabitharevathi@gmail.com>
2016-09-01 12:14:17 +0530

    Add files via upload

---
+++ player53.java
@@ -0,0 +1,12 @@
+package player;
+
+public class player53 {
+	public static void main(String arg[]){
+		String s1=new String("welcome to guvi");
+		String t2=new String("welcome to the world");
+		System.out.println("contains of st1 ="+s1.contains("welcome"));
+
+		System.out.println("contains of st1 ="+t2.contains("guvi"));
+	
+	}
+}
